f13dff6716d69201815e48f400b3e617f1917260
albinaronsson <aronssona@gmail.com>
2025-08-25 12:36:05 +0200

    update config to mount app under /demo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
+// astro.config.mjs
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel";
 
 export default defineConfig({
+  base: "/demo",              // mount the app under /demo
   output: "server",
   adapter: vercel({ imageService: true }),
 });
