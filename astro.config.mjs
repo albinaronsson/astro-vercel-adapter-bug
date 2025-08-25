5e2e462b693c4b53d2c28b4d598e2168a8630ce8
albinaronsson <aronssona@gmail.com>
2025-08-25 21:39:28 +0200

    new attempt att reproducing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
-// astro.config.mjs
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel";
 
 export default defineConfig({
-  base: "/demo",              // mount the app under /demo
   output: "server",
   adapter: vercel({ imageService: true }),
+  assets: true,
 });
